269843649e4c14806317992cd4d7fe0e268b51fc
ShallmentMo <ShallmentMo@gmail.com>
2013-04-03 22:38:51 +0800

    Fix list

--- admin/pages.php
+++ admin/pages.php
@@ -3,7 +3,7 @@
 	{
 		//echo $_POST['page_id'].' '.$_POST['page_title'].' '.$_POST['page_content'].' '.$_POST['page_template'];
 		//save to database
-		//$log->debug($_POST['page_id'].' '.$_POST['page_title'].' '.$_POST['page_content'].' '.$_POST['page_template']);
+		$log->debug($_POST['page_id'].' '.$_POST['page_title'].' '.$_POST['page_content'].' '.$_POST['page_template']);
 		//$log->debug($db->prepare('update pages set page_title= "%1$s",page_content="%2$s",page_template="%3$s" where page_id = %4$d',$_POST['page_title'],$_POST['page_content'],$_POST['page_template'],$_POST['page_id']));
 		if(isset($_POST['page_id']))
 		{
@@ -39,7 +39,22 @@
 				<tr><td>page_title</td><td><input type='text' name='page_title' value='<?php echo get_page_title($_POST['page_id']);?>'/></td></tr>
 				<tr><td>page_content</td><td>
                 <textarea class="mceEditor" name="page_content" cols="50" rows="15"><?php echo get_page_content($_POST['page_id']);?></textarea></td></tr>
-				<tr><td>page_template</td><td><input type='text' name='page_template' value='<?php echo get_page_template($_POST['page_id']);?>'/></td></tr>
+				<tr><td>page_template</td><td>
+				<select class="span3" tabindex="1" name="page_template" id="fileslist">
+							<?php 
+								$tmp=get_page_template($_POST['page_id']);
+								foreach(get_template_files($theme)  as $f)
+								{
+									$f=substr($f,0,-4);
+									if(!in_array($f,array('header','footer')))
+									if($f != $tmp)
+									echo "<option value='$f' >$f</option>";
+									else
+									echo "<option value='$f' selected='selected'>$f</option>";
+								}
+							?>
+				</select>
+				</td></tr>
                 <tr><td></td><td><button class='btn btn-large btn-block' type='submit' formmethod='post' name='action' value='page_save' >save</button></td>
 				</tbody>
         </table>
@@ -63,7 +78,21 @@
 				<tr><td>page_title</td><td><input type='text' name='page_title' value=''/></td></tr>
 				<tr><td>page_content</td><td>
                 <textarea class="mceEditor" name="page_content" cols="50" rows="15"></textarea></td></tr>
-				<tr><td>page_template</td><td><input type='text' name='page_template' value=''/></td></tr>
+				<tr><td>page_template</td><td>
+				<select class="span3" tabindex="1" name="page_template" id="fileslist">
+							<?php
+								foreach(get_template_files($theme)  as $f)
+								{
+									$f=substr($f,0,-4);
+									if(!in_array($f,array('header','footer')))
+									if($f != 'index')
+									echo "<option value='$f' >$f</option>";
+									else
+									echo "<option value='$f' selected='selected'>$f</option>";
+								}
+							?>
+				</select>
+				</td></tr>
                 <tr><td></td><td><button class='btn btn-large btn-block' type='submit' formmethod='post' name='action' value='page_save' >create</button></td>
 				</tbody>
         </table>
